c9fdf7931272e4d790193e3e005f8a159fddf2f3
Любовь Ромашева <romasheva.ld@gmail.com>
2017-10-19 14:15:53 +0300

    Task2

---
+++ src/chapter3/Basic.java
@@ -0,0 +1,7 @@
+package chapter3;
+
+/**
+ * Created by TTT on 18.10.2017.
+ */
+public class Basic {
+}
---
+++ src/chapter3/Employee.java
@@ -0,0 +1,8 @@
+package chapter3;
+
+/**
+ * Created by TTT on 18.10.2017.
+ */
+public class Task2 {
+    
+}
